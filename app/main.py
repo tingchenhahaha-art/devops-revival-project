7ccaf447baad1483eccaac72fc00b88195b180da
tingchenhahaha-art <tingchenhahaha@gmail.com>
2025-08-05 07:54:56 -0700

    Day 2- Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello DevOps Revival!"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
